1c15b3bb69342b822c27af6150582143951e94b4
ckelman <ckelman@u.rochester.edu>
2014-03-14 10:26:30 -0400

    Added ability to create new Posts

--- myBlog/app/controllers/posts_controller.rb
+++ myBlog/app/controllers/posts_controller.rb
@@ -8,4 +8,23 @@ def index
   @posts = Post.all
 end
 
+def new
+  @post = Post.new
+end
+
+def show
+  @post = Post.find(params[:id])
+end
+
+def create
+  @post = Post.new(post_params)
+  @post.save
+  redirect_to @post
+end
+
+private
+  def post_params
+    params.require(:post).permit(:title, :entry)
+  end
+
 end
